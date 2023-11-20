80ba5b19efde68008ce98d9b2b49ff727bacd187
HarshHingu <105966060+HarshHingu@users.noreply.github.com>
2023-11-20 16:43:52 +0530

    Update CategoryProducts.js

--- Client/src/CategoryProducts.js
+++ Client/src/CategoryProducts.js
@@ -15,7 +15,7 @@ const CategoryProducts = () => {
 
 	// const apiURL = "https://fakestoreapi.com/products/category/jewelery";
 
-	const apiURL = `http://localhost:8080/category/${category}`;
+	const apiURL = `ecommerce-orpin-delta.vercel.app/category/${category}`;
 
 	// useEffect hook to fetch the list of products from the API on component mount
 	useEffect(() => {
