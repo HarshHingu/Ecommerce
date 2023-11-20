87d0e88bd41773cafc2f5a75c419c7d830c39247
HarshHingu <105966060+HarshHingu@users.noreply.github.com>
2023-11-20 17:00:24 +0530

    Update LoginForm.jsx

--- Client/src/LoginForm.jsx
+++ Client/src/LoginForm.jsx
@@ -13,7 +13,7 @@ const LoginForm = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:8080/user/login", {
+      const response = await axios.post("https://ecommerce-orpin-delta.vercel.app/user/login", {
         email,
         password,
       });
